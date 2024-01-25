36d19c496eae1963be1d5c79cb9a461b8d9ade23
zizo37 <116495559+zizo37@users.noreply.github.com>
2024-01-25 22:54:57 +0100

    Update Product.js

--- src/Shop/Product.js
+++ src/Shop/Product.js
@@ -1,31 +1,3 @@
-// import React from 'react';
-
-// import './style.css'; 
-
-// export default function Product(props) {
-//   return (
-//     <tr key={props.product.id}>
-//       <td>{props.product.id}</td>
-//       <td>{props.product.title}</td>
-//       <td>{props.product.price}</td>
-//       <td>{props.product.description.slice(0, 100)}....</td>
-//       <td>{props.product.category}</td>
-//       <td style={{ maxWidth: '75px', maxHeight: '75px' }}>
-//         <img
-//           src={props.product.image}
-//           alt={props.product.title}
-//           style={{ maxWidth: '100%', maxHeight: '100%' }}
-//         />
-//       </td>
-//       <td>{props.product.rating.rate}</td>
-//       <td>
-//         <button>supprimer</button>
-//       </td>
-//     </tr>
-//   );
-// }
-
-
 import React from 'react';
 import './style.css'; 
 
